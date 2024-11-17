b27a5a9e499fac5267053c3308827c5ecc56da91
Nick Corfmat <ncorfmat@ucsc.edu>
2024-11-16 17:08:45 -0800

    phaser environment setup

---
+++ src/main.js
@@ -0,0 +1,22 @@
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 800,
+    render: {
+        pixelArt: true
+    },
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: [ Load, Tutorial, Credits, Menu, Cutscene, Select, Play, RumbleWinner, KarateWinner ]
+}
+
+let game = new Phaser.Game(config)
