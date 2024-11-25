6ea9bddb087ec3008bce9d03134891790ba49dc3
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-11-24 21:51:26 -0800

    Implemented single contiguous byte array with AoS format for grid state

--- src/prefabs/Grid.js
+++ src/prefabs/Grid.js
@@ -9,6 +9,15 @@ class Grid {
     // create a map to store cells and their row/col keys
     this.cells = new Map();
 
+    // create DataView object to serve as byte array
+    this.numItems = this.width * this.height;
+    this.bytesPerItem = 8;
+    this.buffer = new ArrayBuffer(this.numItems * this.bytesPerItem);
+    this.byteArray = new DataView(this.buffer);
+
+    this.sunLevel = 0;
+    this.waterLevel = 2;
+
     // track cell selection
     this.selectedCell = null;
     this.lastSelectedCell = null;
@@ -17,14 +26,34 @@ class Grid {
   }
 
   createGrid() {
-    // loop through each cell in the grid
-    for (let row = 0; row < this.width; row++) {
-      for (let col = 0; col < this.height; col++) {
-        // initialize cell and append it to grid map
-        const cell = new Cell(this.scene, row, col, this);
-        this.cells.set(this.generateKey(row, col), cell);
+    let row = 0;
+    let col = 0;
+    for (let i = 0; i < this.numItems; i++) {
+      const offset = i * this.bytesPerItem;
+      // append sun and water level to byte array
+      this.byteArray.setInt32(offset, this.sunLevel, true);
+      this.byteArray.setInt32(offset + 4, this.waterLevel, true);
+
+      this.createCell(offset, row, col);
+
+      col++;
+      if (col >= 8) {
+        col = 0;
+        row++;
       }
     }
+  }
+
+  createCell(offset, row, col) {
+    const cell = new Cell(
+      this.scene,
+      row,
+      col,
+      this.byteArray.getInt32(offset, true),
+      this.byteArray.getInt32(offset + 4, true),
+      this
+    );
+    this.cells.set(this.generateKey(row, col), cell);
   }
 
   selectCell(row, col) {
@@ -78,9 +107,29 @@ class Grid {
   }
 
   updateCellLevels() {
-    this.cells.forEach((cell) => {
-      cell.updateSunLevel();
-      cell.updateWaterLevel();
-    });
+    // update byte array
+    let row = 0;
+    let col = 0;
+    for (let i = 0; i < this.numItems; i++) {
+      const offset = i * this.bytesPerItem;
+
+      this.sunLevel = Phaser.Math.Between(1, 5);
+      this.byteArray.setInt32(offset, this.sunLevel, true);
+
+      const change = Phaser.Math.Between(-1, 1);
+      this.waterLevel = Math.max(0, Math.min(5, this.waterLevel + change));
+      this.byteArray.setInt32(offset + 4, this.waterLevel, true);
+
+      // update cell in grid
+      const cell = this.getCell(row, col);
+      cell.updateSunLevel(this.byteArray.getInt32(offset, true));
+      cell.updateWaterLevel(this.byteArray.getInt32(offset + 4, true));
+
+      col++;
+      if (col >= 8) {
+        col = 0;
+        row++;
+      }
+    }
   }
 }
