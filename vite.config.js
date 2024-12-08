e9aab28674e51bcc78e1cbf4a3133dbb6938941c
Nick Corfmat <ncorfmat@ucsc.edu>
2024-12-07 19:33:24 -0800

    changed file paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cmpm-121-final/",
   plugins: [
     VitePWA({
       manifest: "/manifest.webmanifest",
