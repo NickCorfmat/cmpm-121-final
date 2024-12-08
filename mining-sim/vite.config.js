fe5fe02df46001dd2e1c01a54db8964fe8b7ac38
Mako1688 <128003121+Mako1688@users.noreply.github.com>
2024-12-07 17:09:17 -0800

    Potential app framework

--- mining-sim/vite.config.js
+++ mining-sim/vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mining Sim',
+        short_name: 'MineSim',
+        description: 'A Mining Sim made for CMPM 121 at UCSC.',
+        start_url: '/mining-sim/index.html',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: './assets/demo3.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './assets/demo3.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
 });
