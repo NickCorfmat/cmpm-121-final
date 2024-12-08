7d4a365b5e5ffbb18a3af6b93ade95a5316d2eab
Nick Corfmat <ncorfmat@ucsc.edu>
2024-12-07 22:49:34 -0800

    tweaks

--- src/main.js
+++ src/main.js
@@ -5,20 +5,20 @@ import { PlayScene as Play } from "./scenes/Play";
 import { WinScene as Win } from "./scenes/Win";
 ("use strict");
 let config = {
-  type: Phaser.AUTO,
-  width: 700,
-  height: 400,
-  parent: "phaser-game",
-  render: {
-    pixelArt: true,
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: false,
+    type: Phaser.AUTO,
+    width: 700,
+    height: 400,
+    parent: "phaser-game",
+    render: {
+        pixelArt: true,
     },
-  },
-  scene: [Load, Keys, Play, Win],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+        },
+    },
+    scene: [Load, Keys, Play, Win],
 };
 const game = new Phaser.Game(config);
 const { width, height } = game.config;
